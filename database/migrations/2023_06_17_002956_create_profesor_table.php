98544eed2e3efaa0248b09b100aed4c74968b0d9
Leonel365 <83310075+Leonel365@users.noreply.github.com>
2023-06-16 20:03:27 -0500

    inicio

---
+++ database/migrations/2023_06_17_002956_create_profesor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profesor', function (Blueprint $table) {
+            $table->string('id_profesor', 5)->nullable(false);
+            $table->string('persona', 5)->nullable(false);
+            $table->primary('id_profesor');
+            $table->foreign('persona')->references('id_persona')->on('persona');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profesor');
+    }
+};
